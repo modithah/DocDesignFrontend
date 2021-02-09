b214ee5ff3b5e8b48cdd7bd7b4ee7c9f7770f999
Moditha <moditha@essi.upc.edu>
2021-02-09 11:43:54 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {QueryComponent} from './query/query.component';
+import {ErdComponent} from './erd/erd.component';
+import {DesignComponent} from './design/design.component';
 
+const routes: Routes = [
+  {path:  '', pathMatch:  'full', redirectTo:  'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'query', component: QueryComponent},
+  {path: 'erd', component: ErdComponent},
+  {path: 'design', component: DesignComponent}
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import {ContextMenuModule, TreeViewModule, BeforeOpenCloseMenuEventArgs, MenuEventArgs, MenuItemModel, ContextMenuComponent
+} from '@syncfusion/ej2-angular-navigations';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { QueryComponent } from './query/query.component';
+import { DesignComponent } from './design/design.component';
+import { ErdComponent } from './erd/erd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    QueryComponent,
+    DesignComponent,
+    ErdComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSliderModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HighlightModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+    TreeViewModule,
+    ContextMenuModule,
+    NgSelectModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js'),
+      }
+    }
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
